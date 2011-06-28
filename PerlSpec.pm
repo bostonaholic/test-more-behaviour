adb346a3e0457867400a93e57abade6c32ae90e0
Matthew Boston <matthew.boston@gmail.com>
2011-06-27 22:57:18 -0400

    indentations

--- PerlSpec.pm
+++ PerlSpec.pm
@@ -11,37 +11,37 @@ our @EXPORT = ( @Test::More::EXPORT, qw(it describe) );
 my $spec_desc;
 
 sub it {
-	my $description = shift;
-	my $block       = shift;
-	main::set_up() if main->can('set_up');
-	
-	subtest _construct_description($description) => sub { 
-		plan 'no_plan'; 
-		eval {
-			$block->();
-			1;
-		} or do {
-			fail($@);
-		} ;
-	};
+  my $description = shift;
+  my $block       = shift;
+  main::set_up() if main->can('set_up');
 
-	main::tear_down() if main->can('tear_down');
-	return;
+  subtest _construct_description($description) => sub {
+    plan 'no_plan';
+    eval {
+      $block->();
+      1;
+    } or do {
+      fail($@);
+    };
+  };
+
+  main::tear_down() if main->can('tear_down');
+  return;
 }
 
 sub describe {
-	$spec_desc = shift;
-	my $block = shift;
-	$block->();
-	$spec_desc = undef;
-	return;
+  $spec_desc = shift;
+  my $block = shift;
+  $block->();
+  $spec_desc = undef;
+  return;
 }
 
 sub _construct_description {
-	my ($test_desc) = @_;
-	my $result = $test_desc;
-	$result = "$spec_desc $result" unless ! $spec_desc;
-	return $result;
+  my ($test_desc) = @_;
+  my $result = $test_desc;
+  $result = "$spec_desc $result" unless ! $spec_desc;
+  return $result;
 }
 
 1;
